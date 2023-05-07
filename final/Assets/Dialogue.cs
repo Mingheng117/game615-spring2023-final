e8a60f709a9cd912a975cfc3a1e19a513f133950
Mingheng117 <riderzac@hotmail.com>
2023-05-06 21:43:49 -0400

    upload fixed

--- final/Assets/Dialogue.cs
+++ final/Assets/Dialogue.cs
@@ -21,7 +21,7 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             if (textComponent.text == GetFullyTypedLine(lines[index]))
             {
